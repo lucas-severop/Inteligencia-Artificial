bda0a95b0a52fa83441e3a60e718f296569db0fd
lucas-severop <lucas.portes@ufn.edu.br>
2026-05-13 21:41:53 -0300

    Add files via upload

---
+++ Ia AG 123456789/teste.py
@@ -0,0 +1,64 @@
+# from util import Util
+# from cromossomo import Cromossomo
+
+# lista = []
+# lista.append(Cromossomo("asfas", "andre"))
+# lista.append(Cromossomo("xtfts", "andre"))
+
+# c = Cromossomo("xtfts","andre")
+
+# print(c not in lista)
+
+# for i in lista:
+#     print(i)
+
+
+
+# sPai = "gabriela"
+# sMae = "simonesa"
+
+
+# sFilho1 = sPai[0 : int(len(sPai)/2)] + sMae[int(len(sMae) / 2) : len(sMae)]
+# sFilho2 = sMae[0 : int(len(sMae)/2)] + sPai[int(len(sPai) / 2) : len(sPai)]
+
+# print(sFilho1)
+# print(sFilho2)
+
+# import random
+
+# letras = "abcdefghijklmnopqrstuvxwyz"
+
+# valor = "abcd"
+
+# print(valor[ random.randrange(len(valor)) ])
+# print(letras[ random.randrange(len(letras)) ])
+
+# lista = [1,2,3,4,5]
+# lista[0] = 100
+# print(lista)
+
+# produtos = [
+
+#     ["Ipad", 5000],
+
+#     ['Iphone', 4500],
+
+# ]
+
+# produtos_2 = produtos.copy()
+
+# produtos_2[0][1] = 6000
+
+# print(produtos_2)
+
+# print(produtos)
+
+# import copy
+
+# produtos_3 = copy.deepcopy(produtos)
+
+# produtos_3[1][1] = 5500
+
+# print(produtos_3)
+
+# print(produtos)
